87d50968fd985602ad069c61098cfcbdbf555bb1
Chris Stewart <stewart.chris1234@gmail.com>
2016-05-21 16:47:18 -0500

    Adding boiler plate files

---
+++ project/Build.scala
@@ -0,0 +1,26 @@
+import sbt._
+import Keys._ 
+object BitcoinSSpvNodeBuild extends Build {
+
+  val appName = "bitcoins-spv-node"
+  val appV = "0.0.1" 
+  val scalaV = "2.11.7"
+  val organization = "org.bitcoins.spvnode"
+  val slf4jV = "1.7.5"
+  val logbackV = "1.0.13"
+  val appDependencies = Seq(
+    "org.scalatest" % "scalatest_2.11" % "2.2.0",
+    ("org.bitcoinj" % "bitcoinj-core" % "0.13.3").exclude("org.slf4j", "slf4j-api"),
+    "org.slf4j" % "slf4j-api" % slf4jV /*% "provided"*/,
+    "io.spray" %%  "spray-json" % "1.3.0" withSources() withJavadoc(),
+     "ch.qos.logback" % "logback-classic" % logbackV
+  )
+  
+  val main = Project(appName, file(".")).enablePlugins().settings(
+    version := appV,
+    scalaVersion := scalaV,
+    resolvers += Resolver.sonatypeRepo("releases"),  
+    libraryDependencies ++= appDependencies,
+    scalacOptions ++= Seq("-unchecked", "-deprecation")  
+  )
+} 
