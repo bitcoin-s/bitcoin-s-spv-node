87f842726ec7eca738f52d1c24b885cbc9aa3e9f
Chris Stewart <stewart.chris1234@gmail.com>
2016-05-31 14:36:53 -0500

    Adding trait to represent Message Headers on the p2p network

---
+++ src/main/scala/org/bitcoins/spvnode/headers/RawMessageHeaderSerializer.scala
@@ -0,0 +1,28 @@
+package org.bitcoins.spvnode.headers
+
+import org.bitcoins.core.serializers.RawBitcoinSerializer
+
+/**
+  * Created by chris on 5/31/16.
+  * Reads and writes a message header on the peer-to-peer network
+  * https://bitcoin.org/en/developer-reference#message-headers
+  */
+trait RawMessageHeaderSerializer extends RawBitcoinSerializer[MessageHeader] {
+
+  /**
+    * Transforms a sequence of bytes into a message header
+    * @param bytes the byte representation for a MessageHeader on the peer-to-peer network
+    * @return the native object for the MessageHeader
+    */
+  def read(bytes : List[Byte]) : MessageHeader = ???
+
+  /**
+    * Takes in a message header and serializes it to hex
+    * @param messageHeader the message header to be serialized
+    * @return the hexadecimal representation of the message header
+    */
+  def write(messageHeader: MessageHeader) : String = ???
+
+}
+
+object RawMessageHeaderSerializer extends RawMessageHeaderSerializer
